3bdf2914d6f8122bc90986692fb8ab77eed4e1b1
tian <istancz@gmail.com>
2021-03-14 21:52:42 +0800

    create config

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import {MikroORM} from '@mikro-orm/core'
 import {__prod__} from './constants'
+import {Post} from './entities/Post'
 
 const main = async () => {
   const orm = await MikroORM.init({
+    entities: [Post],
     dbName: 'reddit',
     type: 'postgresql',
-    debug: !__prod__,
-    entities:[]
+    debug: !__prod__
   })
+
+  const post = orm.em.create(Post, {title: 'the first post'})
+  await orm.em.persistAndFlush(post)
+
+  await orm.em.nativeInsert(Post, {title: 'another post'})
 }
 
-console.log('hello world')
+main().catch(err => {
+  console.error(err)
+})
